14fbb14b78b70345304850939c02804610327f8d
Moagi <53832997+Ev1dentSnow@users.noreply.github.com>
2026-03-05 17:28:52 +0200

    added lvgl to dashboard

--- data-acquisition/main/main.c
+++ data-acquisition/main/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "esp_now.h"
 #include <stdio.h>
-#include "driver/gpio.h" 
-#include "freertos/FreeRTOS.h" 
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
@@ -13,7 +13,7 @@ static const char *TAG = "data_acquisition";
 const uint8_t PEER_MAC_ADDRESS = 30 : C9 : 22 : 32 : CF : F4;
 bool peer_connected = false;
 
-void initialize(void)
+void initialize_communication(void)
 {
     esp_now_init();
     esp_now_register_recv_cb(receive_data);
@@ -32,18 +32,13 @@ void send_data(void)
 
 void app_main(void)
 {
-   
-
-
-
-
 
     adc_oneshot_unit_handle_t adc_handle;
-    adc_oneshot_unit_init_cfg_t init_cfg ={
+    adc_oneshot_unit_init_cfg_t init_cfg = {
         .unit_id = ADC_UNIT_1,
 
     };
-    adc_oneshot_new_unit(&init_cfg,&adc_handle);
+    adc_oneshot_new_unit(&init_cfg, &adc_handle);
 
     adc_oneshot_chan_cfg_t rightfront = {
         .atten = ADC_ATTEN_DB_12,
@@ -67,8 +62,7 @@ void app_main(void)
     adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_5, &leftfront);
     adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &rightback);
     adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_7, &leftback);
-    
-    
+
     int rightfront_value;
     adc_oneshot_read(adc_handle, ADC_CHANNEL_4, &rightfront_value);
 
@@ -81,18 +75,11 @@ void app_main(void)
     int leftback_value;
     adc_oneshot_read(adc_handle, ADC_CHANNEL_7, &leftback_value);
 
-    ESP_LOGI(TAG,"%d",rightfront_value);
-    ESP_LOGI(TAG,"%d",leftfront_value);
-    ESP_LOGI(TAG,"%d",rightback_value);
-    ESP_LOGI(TAG,"%d",leftback_value);
-    
-
-
-
-    
-
+    ESP_LOGI(TAG, "%d", rightfront_value);
+    ESP_LOGI(TAG, "%d", leftfront_value);
+    ESP_LOGI(TAG, "%d", rightback_value);
+    ESP_LOGI(TAG, "%d", leftback_value);
 }
-
 
 void reconnect_to_peer(void)
 {
