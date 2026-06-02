e1c9b164eda908fe7d80a70aa4b673b788b4032c
Moagi <53832997+Ev1dentSnow@users.noreply.github.com>
2026-06-02 02:36:42 +0200

    repo cleanup

--- data-acquisition/components/rm68140/rm68140.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "esp_lcd_panel_interface.h"
-#include "esp_lcd_panel_io.h"
-#include "esp_lcd_panel_vendor.h"
-
-esp_err_t rm68140_new_panel(
-    const esp_lcd_panel_io_handle_t io,
-    const esp_lcd_panel_dev_config_t *panel_dev_config,
-    esp_lcd_panel_handle_t *ret_panel);
