3126df3dd992b42b589caee4e23b5f1bef2db210
Ev1dentSnow <munchingfruit@gmail.com>
2026-06-30 16:33:19 +0200

    bootstrapping this ting

---
+++ data_acquisition/components/dashboard/wifi_manager.h
@@ -0,0 +1,17 @@
+#ifndef WIFI_MANAGER_H
+#define WIFI_MANAGER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define WIFI_SSID "BAJA_DASH"
+#define WIFI_PASS "bajadash2026"
+
+void wifi_init_softap(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // WIFI_MANAGER_H
